aeada9f98d6f12c0161c12383add22c62165e63a
juani <juanigm99@gmail.com>
2021-11-17 17:41:53 -0300

    Fix nax for nav in Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Navbar = ({brand}) => {
     return(
-        <nax className="navbar navbar-dark bg-dark">
+        <nav className="navbar navbar-dark bg-dark">
             <div className='container'>
                 <a href="#!" className='navbar-brand'>{brand}</a>
             </div>
-        </nax>
+        </nav>
     )
 }
 
